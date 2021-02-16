75f5e9f669aab9d4f81c938e5889c4ac36e4cb4f
Daniel Otuo-Acheampong <52865422+Danielotuo@users.noreply.github.com>
2021-02-16 14:50:10 -0500

    add base template

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
+API_URL = 'https://financialmodelingprep.com/api/v3/stock/real-time-price/{}'
 
 
 def fetch_price(ticker):
@@ -10,18 +11,20 @@ def fetch_price(ticker):
 
     Args:
         ticker: a string parameter to render just the price of a single stock
-    
+
 
     Returns: the price of the stock as a JSON object
     """
-    data = requests.get('https://financialmodelingprep.com/api/v3/stock/real-time-price/{}'.format(ticker.upper()), params={'apikey': 'demo'}).json()
+    data = requests.get(API_URL.format(ticker.upper()), params={'apikey': 'demo'}).json()
     return data["price"]
+
 
 @app.route('/')
 def home():
-    return "This is the home page. Try going to /stocks/AAPL"
+    return render_template('home.html')
 
-@app.route('/stocks/<ticker>')
+
+@app.route('/stocks/<string:ticker>')
 def view_stock(ticker):
     stock_price = fetch_price(ticker)
-    return render_template('stock_quote.html', ticker=ticker, stock_price=stock_price)
+    return render_template('stock_quote.html', ticker = ticker, stock_price = stock_price)
